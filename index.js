85efb2c090dd40e25071e14a4620dfdea9ad29f0
ahmad <ah167493991@gmail.com>
2025-12-16 11:43:05 +0500

    dd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from test backend");
+  res.send("Hello from test backendz");
 });
 
 app.listen(PORT, () => {
